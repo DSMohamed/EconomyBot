055faffbd064b76fc0f0c13db7dd7746b72d7ab6
DSMohamed <mohamedbuisness2@gmail.com>
2024-10-05 10:55:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
  const client = new Discord.Client({ disableMentions: 'everyone' });
 const Eco = require("quick.eco");
+const keep_alive = require('./keep_alive.js')
 client.eco = new Eco.Manager(); // quick.eco
 client.db = Eco.db; // quick.db
 client.config = require("./botConfig");
